458a0329df321649a6849801c38a03efb58fbfd5
reid <nihilist@users.noreply.github.com>
2019-10-02 23:07:34 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "webextensions": true,
+        "browser": true
+    },
+    "rules": {
+        "no-control-regex": "off",
+        "no-prototype-builtins": "off",
+        "no-restricted-globals": "off",
+        "no-restricted-syntax": "off"
+    },
+    "extends": "airbnb-base"
+};
